25ed8c939fcc6bbc4e4a87aae166bac5003fd2ab
MitchMcKain <mtm120@pitt.edu>
2024-10-28 12:51:11 -0400

    before only calculating most recent stop

--- CTC/mapReader.py
+++ CTC/mapReader.py
@@ -10,15 +10,19 @@ class MapReader():
         file_path = 'C:/Trains C/trains/Train Paths.xlsx'
         df = pd.read_excel(file_path, sheet_name='Green Line')
         
-        for i in range(len(stops)):
+        for i in range(len(stops) + 1):
             # Determine the current and next stop
             if i == 0:
                 current_stop = 'START'
                 next_stop = stops[i]
+            elif i == len(stops):
+                current_stop = stops[i - 1]
+                next_stop = 'END'
             else:
                 current_stop = stops[i - 1]
                 next_stop = stops[i]
-
+            print('current stop = ', current_stop)
+            print('next stop = ', next_stop)
             # Find indices for the current and next stops
             current_stop_index = df[df['Infrastructure'] == current_stop].index
             next_stop_index = df[df['Infrastructure'] == next_stop].index
@@ -29,26 +33,12 @@ class MapReader():
                 end = next_stop_index[0]
                 # Sum the block lengths from start to end
                 authority = df.loc[start:end, 'Block Length (m)'].sum()
+                if i == 0:
+                    authority += 17
                 self.route_authorities_list.append(int(authority))
             else:
                 print(f"Stop '{current_stop}' or '{next_stop}' not found in the data.")
-
-        # Final addition from the last stop to "END"
-        if stops:
-            print('going home')
-            last_stop = stops[-1]
-            last_stop_index = df[df['Infrastructure'] == last_stop].index
-            end_index = df[df['Infrastructure'] == 'END'].index
-
-            if not last_stop_index.empty and not end_index.empty:
-                start = last_stop_index[0]
-                end = end_index[0]
-                # Sum the block lengths from the last stop to "END"
-                authority = df.loc[start:end, 'Block Length (m)'].sum()
-                self.route_authorities_list.append(int(authority))
-            else:
-                print(f"Stop '{last_stop}' or 'END' not found in the data.")
-
+                
         return self.route_authorities_list
 
     
