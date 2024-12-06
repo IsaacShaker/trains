39000ac16f87a5f704b6337699db94ab17ccd85f
MitchMcKain <mtm120@pitt.edu>
2024-12-06 17:14:16 -0500

    merging mtm

--- launcher.py
+++ launcher.py
@@ -4,9 +4,9 @@ from threading import Thread
 import requests
 from api import start_api
 from CTC.ctc import MyWindow
-from TrainModel.TrainModel_UI import Train_UI
-from Train_Controller_SW.User_Interface import Train_Controller_SW_UI
-from TrackModel.FinalUI import TrackUI
+# from TrainModel.TrainModel_UI import Train_UI
+# from Train_Controller_SW.User_Interface import Train_Controller_SW_UI
+# from TrackModel.FinalUI import TrackUI
 # from Train_Controller_HW.TrainControllerHW import Train_Controller_HW_UI
 from PyQt6.QtWidgets import QApplication
 
@@ -15,24 +15,20 @@ class MainApp(QApplication):
     def __init__(self, argv):
         super().__init__(argv)
 
-        #self.ctc = MyWindow()
-        #self.track_controller = MyApp()
-        self.train_model=Train_UI()
-        self.train_controller_sw = Train_Controller_SW_UI(self.train_model.train_list, self.train_model.train_controller_list)
         self.ctc = MyWindow()
-        self.track_controller = MyApp()
-        #self.train_model=Train_UI()
-        #self.train_controller_sw = Train_Controller_SW_UI()
-        #self.train_controller_hw = Train_Controller_HW_UI()
-        self.track_model = TrackUI()
+        # self.track_controller = MyApp()
+        # self.train_model=Train_UI()
+        # self.train_controller_sw = Train_Controller_SW_UI()
+        # self.train_controller_hw = Train_Controller_HW_UI()
+        # self.track_model = TrackUI()
 
 
-        self.track_controller.show()
+        # self.track_controller.show()
         self.ctc.show()
-        #self.train_model.show()
-        #self.train_controller_sw.show()
-        #self.train_controller_hw.show()
-        self.track_model.show()
+        # self.train_model.show()
+        # self.train_controller_sw.show()
+        # self.train_controller_hw.show()
+        # self.track_model.show()
 
     def closeEvent(self, event):
         """Override close event to shutdown Flask server."""
