1176fa3d03bca1b4eef83a82ea9a93904fc54d10
MitchMcKain <mtm120@pitt.edu>
2024-12-12 00:39:36 -0500

    trains stopping in spots for authority

--- TrackController/PLCs/Green_line_HW_PLC_0.py
+++ TrackController/PLCs/Green_line_HW_PLC_0.py
@@ -3,6 +3,12 @@ import time
 def main(stop_event, block_occupancies, switch_suggestions, switches, traffic_lights, crossings, speed_hazard):
     """Main loop of the PLC program that processes inputs and updates outputs."""
 
+    def ABCDEF_occupied():
+        for i in range(1, 29):
+            if block_occupancies[i]:
+                return True
+        return False
+    
     def DEF_occupied():
         for i in range(13, 29):
             if block_occupancies[i]:
@@ -28,24 +34,17 @@ def main(stop_event, block_occupancies, switch_suggestions, switches, traffic_li
     def set_YorZ_hazard(truth_val):
         for i in range(147, 151):
             speed_hazard[i] = truth_val
-
-    def reset_hazard():
-        for i in range(0, len(speed_hazard)):
-            speed_hazard[i] = False
     
     while not stop_event.is_set():
-        
-        reset_hazard()
-
         # A through C
-        for i in range(1, 9):
+        for i in range(1, 13):
             if block_occupancies[i]:
                 # trailing 4 blocks so other trains don't get too close
                 for j in range(1, 5):
-                    speed_hazard[i+j] = True
+                    speed_hazard[i-j] = True
             speed_hazard[i] = False
 
-        # # Sections V - Z
+        # Sections V - Z
         for i in range(117, 151):
             if block_occupancies[i]:
                 # trailing 4 blocks so other trains don't get too close
@@ -53,31 +52,44 @@ def main(stop_event, block_occupancies, switch_suggestions, switches, traffic_li
                     speed_hazard[i-j] = True
             speed_hazard[i] = False
     
-        if DEF_occupied() == False:
-            switches[0] = True
-            switches[1] = False
-            traffic_lights[0] = False
-            traffic_lights[1] = True
-            traffic_lights[2] = True
-            traffic_lights[3] = False
-
-            set_A_hazard(False)
-            set_YorZ_hazard(False)
-
-            # if trains are at both ends of section DEF, give priority over trains at section Z
-            if A_occupied() and YorZ_occupied():
-                set_A_hazard(True) # stops trains at A
-        elif DEF_occupied() == True and not block_occupancies[1] and not block_occupancies[150]:
+        if ABCDEF_occupied() == False:
             switches[0] = False
             switches[1] = True
-            traffic_lights[0] = True
-            traffic_lights[1] = False
+            traffic_lights[0] = False
+            traffic_lights[1] = True
             traffic_lights[2] = False
             traffic_lights[3] = True
 
-            # stop other trains from entering section
+            #set_A_hazard(False)
+            set_YorZ_hazard(False)
+
+            # if trains are at both ends of section DEF, give priority over trains at section Z
+            #if A_occupied() and YorZ_occupied():
+                #set_A_hazard(True) # stops trains at A
+        elif (DEF_occupied() == False and not block_occupancies[150]) or ABCDEF_occupied() == True:
+            #switches[0] = True
+            switches[1] = False
+            #traffic_lights[0] = True
+            #traffic_lights[1] = False
+            traffic_lights[2] = True
+            traffic_lights[3] = False
             set_YorZ_hazard(True)
-            set_A_hazard(True)
+
+            if DEF_occupied() == True and not block_occupancies[1]:
+                switches[0] = False
+                traffic_lights[0] = False
+                traffic_lights[1] = True
+
+            elif DEF_occupied() == False:
+                switches[0] = True
+                traffic_lights[0] = True
+                traffic_lights[1] = False
+
+
+            # stop other trains from entering section
+            #set_YorZ_hazard(True)
+            #set_A_hazard(True)
+
 
         # check 3 blocks behind crossing and block of crossing (4 total blocks)
         crossings[0] = False # Default is up
@@ -88,7 +100,8 @@ def main(stop_event, block_occupancies, switch_suggestions, switches, traffic_li
             if block_occupancies[19 + i]: # block 19 is the railroad crossing
                 crossings[0] = True # Put the crossing down
                 break
-    
-        break
+
         # Simulate delay to avoid CPU hogging
+        break
         time.sleep(0.1)
+
